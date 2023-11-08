7a41eebcb42e41277bd77a3c4ea99dcc36680908
ibra2407 <mehloldex@gmail.com>
2023-11-08 18:14:27 +0800

    follow chin sng

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {ReactDOM} from 'react-dom/client'
-// import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React>
-    <App />
-  </React>
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
 
 // used to be Main.jsx
