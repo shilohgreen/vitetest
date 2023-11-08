6b72989b928f3c7230f62907b620ddf6b323c78b
ibra2407 <mehloldex@gmail.com>
2023-11-08 18:12:56 +0800

    remove strict mode

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import {ReactDOM} from 'react-dom/client'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React>
     <App />
-  </React.StrictMode>
+  </React>
 );
 
 // used to be Main.jsx
